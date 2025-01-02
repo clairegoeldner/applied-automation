3f5e9397724b4f9d817917e7bcd44b187e69b57e
Claire Goeldner <crgoeldner@students.ntc.edu>
2025-01-02 11:34:47 -0600

    Update API URL

--- ReactWebsite/src/utils/ApiUtil.js
+++ ReactWebsite/src/utils/ApiUtil.js
@@ -1,10 +1,10 @@
 // const url = "https://localhost:7001";
-const url = "https://applied-automation-api.azurewebsites.net";
+const url = "https://api-applied-automation.azurewebsites.net";
 
 export async function GetToken(username, password) {
     const response = await fetch(url + "/auth/token", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json", "Accept": "*/*" },
         body: JSON.stringify({ username, password })
     });
 
